1fbe9bfa7495abcbdaf77a37958c31dbfb005430
Maigi Rannamets <rannamets.maigi@gmail.com>
2024-07-01 19:39:25 +0300

    Create hover effect for div's

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 'use strict';
 
+// Creating div's
 document.addEventListener('DOMContentLoaded', () => {
   const divContainer = document.querySelector('.grid');
+  let newDiv;
 
   for (let i = 0; i < 16 * 16; i++) {
-    const newDiv = document.createElement('div');
+    newDiv = document.createElement('div');
     newDiv.classList.add('grid-item');
     divContainer.appendChild(newDiv);
   }
+
+  // Hover effect
+  const divItems = document.querySelectorAll('.grid-item');
+  divItems.forEach((item) => {
+    item.addEventListener('mouseover', () => {
+      item.style.backgroundColor = '#000';
+    });
+  });
 });
