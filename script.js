9cb10b60e541c9a86514f6da7be393b8c6f50609
Maigi Rannamets <rannamets.maigi@gmail.com>
2024-07-09 17:50:51 +0300

    Add div width and height calculations

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 'use strict';
 
 const containerForGrids = document.querySelector('.container-grids');
-const gridButton = document.querySelector('.select-grids');
+const gridBtn = document.querySelector('.grids-btn');
+const resetBtn = document.querySelector('.reset-btn');
 
 // Creating grids
-
-gridButton.addEventListener('click', () => {
+gridBtn.addEventListener('click', () => {
   const grids = +prompt('Enter number of grids between 16-100: ');
-  console.log(grids);
 
-  for (let i = 1; i <= grids; i++) {
-    const newDiv = document.createElement('div');
+  if (grids < 16 || grids > 100) {
+    alert('Not a valid number! Try again.');
+    return;
+  }
+  let newDiv;
+  for (let i = 1; i <= grids * grids; i++) {
+    newDiv = document.createElement('div');
     newDiv.classList.add('divs');
+    newDiv.style.width = `calc(100% / ${grids})`;
+    newDiv.style.height = `calc(100% / ${grids})`;
     containerForGrids.appendChild(newDiv);
   }
+
+  newDiv.forEach((item) =>
+    item.addEventListener('mouseover', () => {
+      item.style.backgroundColor = '#000';
+    })
+  );
 });
+
+function resetContainer() {
+  containerForGrids.innerHTML = '';
+}
+
+resetBtn.addEventListener('click', resetContainer);
