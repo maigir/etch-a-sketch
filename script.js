dcb6d2a66aaec9b936eb66adfb8a314efaa25514
Maigi Rannamets <rannamets.maigi@gmail.com>
2024-07-09 20:41:08 +0300

    Set default grid value on reset button

--- script.js
+++ script.js
@@ -3,33 +3,36 @@
 const containerForGrids = document.querySelector('.container-grids');
 const gridBtn = document.querySelector('.grids-btn');
 const resetBtn = document.querySelector('.reset-btn');
+const eraseBtn = document.querySelector('.erase-btn');
+const DEFAULT_GRID = 16;
 
 // Creating grids
+function createGrid(size) {
+  containerForGrids.innerHTML = '';
+
+  for (let i = 1; i <= size * size; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('divs');
+    newDiv.style.width = `calc(100% / ${size})`;
+    newDiv.style.height = `calc(100% / ${size})`;
+    newDiv.addEventListener('mouseover', () => {
+      newDiv.style.backgroundColor = 'black';
+    });
+    containerForGrids.appendChild(newDiv);
+  }
+}
+
 gridBtn.addEventListener('click', () => {
   const grids = +prompt('Enter number of grids between 16-100: ');
-
   if (grids < 16 || grids > 100) {
     alert('Not a valid number! Try again.');
     return;
   }
-  let newDiv;
-  for (let i = 1; i <= grids * grids; i++) {
-    newDiv = document.createElement('div');
-    newDiv.classList.add('divs');
-    newDiv.style.width = `calc(100% / ${grids})`;
-    newDiv.style.height = `calc(100% / ${grids})`;
-    containerForGrids.appendChild(newDiv);
-  }
-
-  newDiv.forEach((item) =>
-    item.addEventListener('mouseover', () => {
-      item.style.backgroundColor = '#000';
-    })
-  );
+  createGrid(grids);
 });
 
 function resetContainer() {
-  containerForGrids.innerHTML = '';
+  createGrid(DEFAULT_GRID);
 }
 
 resetBtn.addEventListener('click', resetContainer);
